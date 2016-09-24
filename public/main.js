c67e1329d0e3e910542282ce95320cac00efe91d
coreygreen1108 <coreygreen1108@gmail.com>
2016-09-24 12:24:48 -0400

    Added small front end jquery for interactivity

---
+++ public/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#search').on('click', function(){
+		let system = $('#console').val();
+		let command = $('#command').val();
+		let target = $('#target').val();
+		let url = "/godinfo/" + system + '/' + command + '/' + target;
+		window.location.replace(url);
+	});
+});
