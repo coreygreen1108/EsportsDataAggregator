1b8d2f9559e9332d521c5600fa15b90f04927521
coreygreen1108 <coreygreen1108@gmail.com>
2017-05-16 14:58:16 -0400

    set up sequelize configuration files

--- db/_db.js
+++ db/_db.js
@@ -1,6 +1,7 @@
 var Sequelize = require('sequelize');
 
 var db = new Sequelize('postgres://localhost:5432/smiteVizDev', {
+  dialect: 'postgres',
   logging: false
 });
 
---
+++ migrations/20170516175601-test-god-migration.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let co = require('co');
+let db = require('../db');
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return co(function *() {
+      var god = yield db.model('God').create({
+        name: 'Terra'
+      })
+      console.log(god); 
+    })
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
