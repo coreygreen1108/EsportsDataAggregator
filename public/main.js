2f07b8defe80d3f40af70d7e58ede182769f5e3d
Scott Anderson <scottya1992@gmail.com>
2017-06-05 15:30:59 -0400

    removing public/main.js

--- public/main.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function(){
-// 	$('#search').on('click', function(){
-// 		let system = $('#console').val();
-// 		let command = $('#command').val();
-// 		let target = $('#target').val();
-// 		let url = "/godinfo/" + system + '/' + command + '/' + target;
-// 		window.location.replace(url);
-// 	});
-// });
